6a755e63190031ebed1d208bfb9ddacc39a65267
Rubén Illodo Brea <rubenillodobrea@gmail.com>
2017-01-11 13:57:52 +0800

    Simplified Table/Table/index.jsx

--- src/components/Table/Table/index.jsx
+++ src/components/Table/Table/index.jsx
@@ -3,27 +3,16 @@ import React from 'react';
 import { Tr } from '../';
 
 const Table = (props) => <table {...props}>
-  {(() => {
-    const rows = [];
-    const createCellsInRow = rowIdx => {
-      const cells = [];
-      for (const colIdx of Array(props.colCount).keys()) {
-        cells.push(props.children(rowIdx, colIdx));
-      }
-      return cells;
-    };
-    for (const rowIdx of Array(props.rowCount).keys()) {
-      rows.push(<Tr>
-        {createCellsInRow(rowIdx)}
-      </Tr>);
-    }
-    return rows;
-  })()}
+  {
+    Array(props.rowCount).map((_item, rowIdx) => <Tr key={rowIdx}>
+      {Array(props.colCount).map((__item, colIdx) => props.children(rowIdx, colIdx))}
+    </Tr>)
+  }
 </table>;
 
 Table.propTypes = {
-  rowCount: React.PropTypes.bool.isRequired,
-  colCount: React.PropTypes.bool.isRequired,
+  rowCount: React.PropTypes.number.isRequired,
+  colCount: React.PropTypes.number.isRequired,
   children: React.PropTypes.func.isRequired,
 };
 
