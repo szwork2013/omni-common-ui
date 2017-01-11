d62ae73218bc2d689fbe50c684eefed229d7f585
Rubén Illodo Brea <rubenillodobrea@gmail.com>
2017-01-11 13:57:37 +0800

    Removed useless test files

--- src/components/Table/spec.js
+++
@@ -1,8 +0,0 @@
-// import Reactable from 'reactable';
-// import { expect } from 'chai';
-// import Table from './';
-// import TableComponent from './Table';
-// import ExpandableComponent from './Expandable';
-
-describe('Table', () => {
-});
